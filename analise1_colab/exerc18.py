fbef8e4e3cec88a38cee575cfe7b6d9c9fdafcb6
Luis André <andrerod15@gmail.com>
2025-05-05 19:27:50 -0300

    #16 - feat(): add exerc18

---
+++ analise1_colab/exerc18.py
@@ -0,0 +1,4 @@
+"""## Exercício 18: Retorne 'Adulto' se a idade for maior ou igual a 18, senão 'Menor de idade'."""
+
+idade = int(input('Digite a idade: '))
+print('Adulto' if idade >= 18 else 'Menor de Idade')
