416abcca6b19747e0f81171f0d730704aed0e5d4
Luis André Rodrigues <andrerod15@gmail.com>
2025-05-05 15:41:55 -0300

    add_exerc10 #8

---
+++ exerc10.py
@@ -0,0 +1,8 @@
+#Exercício 10: Peça ao usuário um número e exiba 'Alto' se for maior que 100 e 'Baixo' se for menor.
+
+num = int(input('Digite um número: '))
+if num >= 100:
+  print('Alto')
+else:
+  print('Baixo')
+  
