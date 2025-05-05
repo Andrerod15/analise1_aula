e3d63b09e485df51f9e33f4987f2e1554586a9f9
Luis André <andrerod15@gmail.com>
2025-05-05 19:42:24 -0300

    #17 - feat(): add exerc19

---
+++ analise1_colab/exerc19.py
@@ -0,0 +1,9 @@
+Exercício 19: Verifique se um número é maior que 100 e retorne 'Grande' ou 'Pequeno'.
+
+num = int(input('Digite um número: '))
+if num > 100:
+  print('Grande')
+else:
+  print('Pequeno')
+
+
