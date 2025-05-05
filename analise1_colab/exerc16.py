0fa0829b156243b43b57009ecf5d18cb0611fc59
Luis André <andrerod15@gmail.com>
2025-05-05 19:25:43 -0300

    #14 - feat(): add exerc16

---
+++ analise1_colab/exerc16.py
@@ -0,0 +1,15 @@
+"""## Exercício 16: Verifique se um número é par ou ímpar usando if ternário."""
+
+num = int(input('Digite um número: '))
+print('Par' if num % 2 == 0 else 'Ímpar')
+
+#Verifique o(s) número(s) apresentados;
+#Faça a divisão de cada um por 2;
+#Se o resultado for um número inteiro diga que é par;
+#Se for um número decimal, que é ímpar.
+
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+  print('Par')
+else:
+  print('Ímpar')
