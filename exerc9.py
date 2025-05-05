42ca81999bde831c623ae4dc580a851824efb123
Luis André Rodrigues <andrerod15@gmail.com>
2025-05-05 14:40:42 -0300

    add_exerc9

---
+++ exerc9.py
@@ -0,0 +1,7 @@
+#Exercício 9: Verifique se um número está entre 10 e 50.
+
+num = int(input('Digite um número: '))
+if num >= 10 and num <= 50:
+  print('O valor está entre 10 e 50')
+else:
+  print('O valor não está entre 10 e 50')
