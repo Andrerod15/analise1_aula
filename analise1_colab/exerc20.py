1ee1da30e7b6fd18c2e0a9e90f060b2730a2cee7
Luis André <andrerod15@gmail.com>
2025-05-07 18:23:30 -0300

    #18 - feat(): add exerc20

---
+++ analise1_colab/exerc20.py
@@ -0,0 +1,8 @@
+#Exercício 20: Retorne 'Aprovado' se a nota for maior ou igual a 7, senão 'Reprovado'.
+
+num = float(input('Nota do aluno: '))
+if num >= 7:
+  print('Aprovado')
+else:
+  print('Reprovado')
+  
